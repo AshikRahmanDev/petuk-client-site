ab6fdcb9ec7520f54ebfae2ec69e5919737a83de
MohammdAshik <mohammadashik797@gmail.com>
2022-11-09 01:39:33 +0600

    allmenu route added and display allmeals(all services)

--- src/Pages/Home/Menu/Menu.jsx
+++ src/Pages/Home/Menu/Menu.jsx
@@ -11,16 +11,19 @@ const Menu = () => {
       .then((data) => setMeals(data));
   }, []);
   return (
-    <div className="w-[97%] mx-auto my-5">
+    <div className="w-[97%] mx-auto mt-8 mb-5">
       <h1 className="text-3xl font-bold normal-case my-5">Popular Menu</h1>
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {meals.map((meal) => (
           <MealCard key={meal._id} meal={meal} />
         ))}
       </div>
       <div className="my-5 text-center">
-        <Link className="btn bg-orange-500 border-0 w-[200px] hover:bg-white hover:text-orange-500 hover:border-2 hover:border-orange-500">
+        <Link
+          to={"/allMenu"}
+          className="btn bg-orange-500 border-0 w-[200px] hover:bg-white hover:text-orange-500 hover:border-2 hover:border-orange-500"
+        >
           View All
         </Link>
       </div>
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layout/Root";
 import AddMeal from "../Pages/AddMeal/AddMeal";
+import AllMenu from "../Pages/AllMenu/AllMenu";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
@@ -12,6 +13,11 @@ export const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },
       { path: "/addMeal", element: <AddMeal /> },
+      {
+        path: "/allMenu",
+        loader: () => fetch("http://localhost:5000/menu/meals"),
+        element: <AllMenu />,
+      },
       { path: "/login", element: <Login /> },
       { path: "/register", element: <Register /> },
     ],
