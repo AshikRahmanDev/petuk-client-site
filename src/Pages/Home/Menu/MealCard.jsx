6fa1f9bcec74fb79da39678b3ae38539359dd320
MohammdAshik <mohammadashik797@gmail.com>
2022-11-09 20:23:33 +0600

    clicking on the image of the meals, the picture viewed on full screen features added

--- src/Pages/Home/Menu/MealCard.jsx
+++ src/Pages/Home/Menu/MealCard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const MealCard = ({ meal }) => {
   const { name, picture, price, details, _id } = meal;
   return (
     <div className="mx-auto p-3 border border-orange-400 rounded-lg">
-      <img
+      {/* <img
         className="w-[400px] h-[250px] object-cover rounded-lg"
         src={picture}
         alt=""
-      />
+      /> */}
+      <PhotoProvider>
+        <PhotoView src={picture}>
+          <img
+            className="w-[400px] h-[250px] object-cover rounded-lg"
+            src={picture}
+            alt=""
+          />
+        </PhotoView>
+      </PhotoProvider>
       <div>
         <h4 className="text-3xl font-bold normal-case">{name}</h4>
         <p className="my-2 text-gray-600">
--- src/Pages/MealDetails/ReviewSection.jsx
+++ src/Pages/MealDetails/ReviewSection.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../Context/AuthProvider";
 
 const ReviewSection = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  const { displayName, email, photoURL } = user;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const reviewMessage = form.review.value;
+    const review = {
+      reviewMessage,
+      displayName,
+      email,
+      photoURL,
+    };
+    console.log(review);
+  };
   return (
     <div className="border-2 my-6 p-3">
       <h1 className="text-4xl font-bold">Customer Reviews</h1>
@@ -8,20 +24,18 @@ const ReviewSection = () => {
         <div className="col-span-3"></div>
         <div className="col-span-1">
           <h4 className="text-xl font-bold">Review This Meal</h4>
-          <form>
+          <form onSubmit={handleSubmit}>
             <textarea
-              name="reviewMessage"
+              name="review"
               className="input border-gray-400 h-[150px] my-2"
               cols="30"
               rows="10"
             ></textarea>
-            <div>
-              <input
-                type="submit"
-                className="btn rounded-3xl bg-orange-500 border-0"
-                value="Post your review"
-              />
-            </div>
+            <input
+              type="submit"
+              className="btn rounded-3xl bg-orange-500 border-0"
+              value="Post your review"
+            />
           </form>
         </div>
       </div>
